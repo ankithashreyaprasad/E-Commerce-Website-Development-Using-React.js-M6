4610fbafb4444af7c412b45ce6c692ccfbde33d5
Ankitha Shreya Prasad <ankithashreyaprasad@gmail.com>
2026-06-04 21:07:50 +0530

    Fix framer motion TS variants issue

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -53,16 +53,15 @@ const ProductList = () => {
     };
 
     const item = {
-        hidden: { opacity: 0, y: 30 },
-        show: {
-            opacity: 1,
-            y: 0,
-            transition: {
-                duration: 0.8,
-                ease: [0.22, 1, 0.36, 1]
-            }
+    hidden: { opacity: 0, y: 30 },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.8
         }
-    };
+    }
+};
 
     return (
         <div className="max-w-7xl mx-auto">
